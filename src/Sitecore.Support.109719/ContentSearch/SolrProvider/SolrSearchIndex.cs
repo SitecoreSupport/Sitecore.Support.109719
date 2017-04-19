1fa536f44e355944c2bb7ce1aa428e96799ee1bc
sc-spl <spl@sitecore.net>
2017-04-19 16:14:51 +0300

    Initial commit

---
+++ src/Sitecore.Support.109719/ContentSearch/SolrProvider/SolrSearchIndex.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.Maintenance;
+
+namespace Sitecore.Support.ContentSearch.SolrProvider
+{
+    public class SolrSearchIndex : Sitecore.ContentSearch.SolrProvider.SolrSearchIndex
+    {
+        protected override void PerformRefresh(IIndexable indexableStartingPoint, IndexingOptions indexingOptions, CancellationToken cancellationToken)
+        {
+            if (base.ShouldStartIndexing(indexingOptions))
+            {
+                using (IProviderUpdateContext context = this.CreateUpdateContext())
+                {
+                    foreach (IProviderCrawler crawler in base.Crawlers)
+                    {
+                        crawler.RefreshFromRoot(context, indexableStartingPoint, indexingOptions, cancellationToken);
+                    }
+                    context.Commit();
+                }
+                //this.OptimizeIndex();
+            }
+        }
+
+        public SolrSearchIndex(string name, string core, IIndexPropertyStore propertyStore, string @group) : base(name, core, propertyStore, @group)
+        {
+        }
+
+        public SolrSearchIndex(string name, string core, IIndexPropertyStore propertyStore) : base(name, core, propertyStore)
+        {
+        }
+    }
+}
---
+++ src/Sitecore.Support.109719/ContentSearch/SolrProvider/SwitchOnRebuildSolrSearchIndex.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.Maintenance;
+
+namespace Sitecore.Support.ContentSearch.SolrProvider
+{
+    public class SwitchOnRebuildSolrSearchIndex : Sitecore.ContentSearch.SolrProvider.SwitchOnRebuildSolrSearchIndex
+    {
+        public SwitchOnRebuildSolrSearchIndex(string name, string core, string rebuildcore, IIndexPropertyStore propertyStore) : base(name, core, rebuildcore, propertyStore)
+        {
+        }
+
+        protected override void PerformRefresh(IIndexable indexableStartingPoint, IndexingOptions indexingOptions, CancellationToken cancellationToken)
+        {
+            if (base.ShouldStartIndexing(indexingOptions))
+            {
+                using (IProviderUpdateContext context = this.CreateUpdateContext())
+                {
+                    foreach (IProviderCrawler crawler in base.Crawlers)
+                    {
+                        crawler.RefreshFromRoot(context, indexableStartingPoint, indexingOptions, cancellationToken);
+                    }
+                    context.Commit();
+                }
+                //this.OptimizeIndex();
+            }
+        }
+    }
+}
